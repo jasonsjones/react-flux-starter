9b94758a38faae039f974b3e3a9ce40a1d2c10e8
Jason <jasonjones@salesforce.com>
2017-07-28 11:29:27 -0700

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <div className='container'>
+                <h1> Hello React! </h1>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname + '/dist/'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        inline: true,
+        port: 8080,
+        historyApiFallback: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015', 'react']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+}
