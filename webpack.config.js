d5df358517bfaff9c0a188f5292c8cc88f1494aa
Jason <jasonjones@salesforce.com>
2017-08-08 10:02:57 -0700

    Add exclude prop for css rule

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,7 @@ module.exports = {
             },
             {
                 test: /\.css$/,
+                exclude: /node_modules/,
                 use: ['style-loader', 'css-loader']
             }
         ]
